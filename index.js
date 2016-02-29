01448d30bf68879baf7f7e84aaca5d82f0f4c510
M.C. van den Boogaart <scola@kvdb.name>
2016-02-29 20:10:10 +0100

    Put actual capabilities into separate module

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict';
 
 const DI = require('@scola/di');
+const Abstract = require('./lib/abstract.js');
 const Dispatcher = require('./lib/dispatcher.js');
 
 class Module extends DI.Module {
   configure() {
-    this.inject(Dispatcher).with(
-      this.object({
-        hairline: this.singleton(require('./lib/capabilities/hairline'))
-      }),
-      this.value(window)
-    );
+    if (typeof window !== 'undefined') {
+      this.inject(Dispatcher)
+        .insertArgument(1, this.value(window));
+    }
   }
 }
 
 module.exports = {
-  Module,
-  Dispatcher
+  Abstract,
+  Dispatcher,
+  Module
 };
